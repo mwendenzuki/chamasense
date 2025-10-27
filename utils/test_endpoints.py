f5a38d4f717969b0e04b32c7f40e41e062a92f1d
mwendenzuki <nzukiruthm@gmail.com>
2025-10-27 16:17:21 +0300

    Updated test bse URLS

--- utils/test_endpoints.py
+++ utils/test_endpoints.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "http://127.0.0.1:5000"
+BASE_URL = "https://chamasense.onrender.com"
 
 def safe_print(label, res):
     print(f"{label}: {res.status_code}")
--- utils/test_forecast.py
+++ utils/test_forecast.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:5000/forecast_savings"
+url = "https://chamasense.onrender.com/forecast_savings"
 
 data = {
     "income": 50000,
